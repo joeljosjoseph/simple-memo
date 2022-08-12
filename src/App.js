89f6c5a413b115d61b46c6a3b7564ce52de41044
Joel Jos Joseph <joeljosjoseph12@gmail.com>
2022-08-12 22:20:49 +0530

    Added some Css to the input form

--- src/App.js
+++ src/App.js
@@ -26,26 +26,37 @@ function App() {
   };
 
   return (
-    <>
-      <div className="container">
-        <form onSubmit={(e) => handleSubmit(e)} className="container-form">
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
-          <textarea value={input} onChange={(e) => setInput(e.target.value)} />
-          <button className="button-save">Save</button>
-        </form>
+    <div className="container">
+      <form onSubmit={(e) => handleSubmit(e)} className="container-form">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Add Title"
+          className="form-input"
+        />
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          maxLength="250"
+          placeholder="Add content"
+          className="form-input"
+        />
+        <button className="button-save">Add Memo</button>
+      </form>
+      <div className="memo-container">
+        {memos.map((memo, i) => {
+          return (
+            <div className="memo-div">
+              <button className="button-delete" onClick={() => handleDelete(i)}>
+                Delete
+              </button>
+              <p className="">{memo.title}</p>
+              <p>{memo.body}</p>
+            </div>
+          );
+        })}
       </div>
-      {memos.map((memo, i) => {
-        return (
-          <div className="memo-container">
-            <button className="button-delete" onClick={() => handleDelete(i)}>
-              Delete
-            </button>
-            <p className="">{memo.title}</p>
-            <p>{memo.body}</p>
-          </div>
-        );
-      })}
-    </>
+    </div>
   );
 }
 
